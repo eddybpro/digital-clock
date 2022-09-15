08594f158f8ad2f70f072b7df729639262064cdc
eddybpro <abdelilahdb154@gmail.com>
2022-09-15 09:52:50 -0700

    digital clock

---
+++ script.js
@@ -0,0 +1,28 @@
+const clock = document.getElementById('clock');
+
+function showTime(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = 'AM';
+
+    if(h == 12){
+        h = 0; //time format;
+    }
+
+    if(h > 12){
+        h = h - 12;
+        session = 'PM';
+    }
+
+
+    h = (h < 10) ? '0' + h: h;
+    m = (m < 10) ? '0' + m: m;
+    s = (s < 10) ? '0' + s: s;
+
+    clock.innerHTML= h +' : ' + m + ' : ' + s + ' ' + session;
+
+    setInterval(showTime, 1000);
+}
+showTime()
